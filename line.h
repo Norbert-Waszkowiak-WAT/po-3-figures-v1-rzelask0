5e4f76c1d62636a68f694a1acf279410fe9a6d6e
rzelask0 <166538354+rzelask0@users.noreply.github.com>
2024-12-04 19:21:52 +0000

    skibidi mostek bez grup

---
+++ line.h
@@ -0,0 +1,21 @@
+#ifndef LINE_H
+#define LINE_H
+#include <iostream>
+#include <string>
+#include "point.h"
+
+class Line {
+private:
+    Point a;
+    Point b;
+public:
+    Line(Point a, Point b);
+    Line(const Line &other);
+    bool equals(const Line &other);
+    void flip();
+    void move(double x, double y);
+    string toString();
+    
+
+};
+#endif
---
+++ point.h
@@ -0,0 +1,33 @@
+#ifndef POINT_H
+#define POINT_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Point{
+private:
+    double x;
+    double y;
+public:
+    Point(double x, double y);
+
+    Point(const Point &other);
+
+    bool equals(const Point &other);
+
+    double getX();
+
+    double getY();
+
+    void flip();
+
+    void move(double x, double y);
+
+    string toString();
+
+    double distanceTo(const Point &other);
+
+    
+};
+#endif
